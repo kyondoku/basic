40dbe9a5e5b6d04c1813d099e5d65d65c64f7654
kyondoku <kyondoku@gmail.com>
2023-03-21 01:11:36 +0900

    전달받은 prop으로 구분하여 특정요소 보여주기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
-export default function Profile({image, name, title}) {
+export default function Profile({image, name, title, isNew}) {
+
     return (
         <div className="profile">
             <img className="photo" 
                 src={image} alt='avatar'/>
+            {isNew && <span className="new">new</span>}
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
