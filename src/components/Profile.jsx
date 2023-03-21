260e41d6d8a04155ea5cbb7a65e3679e2def02fe
kyondoku <kyondoku@gmail.com>
2023-03-22 01:01:25 +0900

    img부분도 컴포넌트화하기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,16 @@
+import Avatar from "./Avatar";
+
 export default function Profile({image, name, title, isNew}) {
 
     return (
         <div className="profile">
-            <img className="photo" 
+            {/* <img className="photo" 
                 src={image} alt='avatar'/>
-            {isNew && <span className="new">new</span>}
+            {isNew && <span className="new">new</span>} */}
+            <Avatar 
+                image={image}
+                isNew={isNew}
+            />
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
