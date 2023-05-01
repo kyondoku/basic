4ce9e52b0cc1bb1aad467f6f6507b1ae1efc90ac
kyondoku <kyondoku@gmail.com>
2023-05-02 01:22:47 +0900

    추가,삭제하기 / 로직 분리하기

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -2,19 +2,40 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function AppMentors() {
-    const [person, setPerson] = useState({
-        name: '엘리',
-        title: '개발자',
-        mentors : [
-            {
-                name: '밥',
-                title: '시니어개발자',
-            },
-            {   name: '제임스',
-                title: '시니어개발자',
-            }
-        ]
-    })
+    const [person, setPerson] = useState(initialPerson);
+    const handleUpdate = () => {
+        const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+        const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+
+        setPerson(person => ({
+            ...person, 
+            mentors : person.mentors.map((mentor) => {
+                if(mentor.name == prev) {
+                    return {...mentor, name: current};
+                } 
+                return mentor;
+            })
+        }));
+    }
+
+    const handleAdd = () => {
+        const name = prompt(`추가할 멘토의 이름은 무엇인가요?`);
+        const title = prompt(`추가할 멘토의 직업은 무엇인가요?`);
+
+        setPerson(person => ({
+            ...person, 
+            mentors : [...person.mentors, { name, title }]
+        }));
+    }
+
+    const handleDelete = () => {
+        const name = prompt(`누구를 삭제하고 싶은가요?`);
+
+        setPerson(person => ({
+            ...person, 
+            mentors : person.mentors.filter(m => m.name !== name),
+        }));
+    }
     
     return (
         <div>
@@ -30,24 +51,34 @@ export default function AppMentors() {
                 ))}
             </ul>
             <button
-                onClick={() => {
-                    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-                    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-
-                    setPerson(person => ({
-                        ...person, 
-                        mentors : person.mentors.map((mentor) => {
-                            if(mentor.name == prev) {
-                                return {...mentor, name: current};
-                            } 
-                            return mentor;
-                        })
-                    }));
-                }}
+                onClick={handleUpdate}
             >
                 멘토의 이름을 바꾸기
+            </button>
+            <button
+                onClick={handleAdd}
+            >
+                멘토 추가하기
+            </button>
+            <button
+                onClick={handleDelete}
+            >
+                멘토 삭제하기
             </button>
         </div>
     );
 }
 
+const initialPerson = {
+    name: '엘리',
+    title: '개발자',
+    mentors : [
+        {
+            name: '밥',
+            title: '시니어개발자',
+        },
+        {   name: '제임스',
+            title: '시니어개발자',
+        }
+    ]
+}
